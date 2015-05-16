2046a92800bc48a109736fd03b1b6c901c9266cc
NewWorldOrder <nwokedi@gmail.com>
2015-05-15 22:39:39 -0700

    Solution for Project 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix - constructs an "object" with set, get, set_inverse, and get_inverse 
+##                   as methods
+## cacheSolve - computes an inverse matrix only if there isn't one saved already 
 
-## Write a short comment describing this function
-
+## Construct an object that will represent a matrix and its inverse
+## 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse_matrix <- NULL
+  set <- function(new_matrix){
+    x <<- new_matrix
+    inverse_matrix <<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(i_matrix) inverse_matrix <<- i_matrix
+  get_inverse <- function() inverse_matrix 
+  list(set=set, get=get, set_inverse=set_inverse, get_inverse=get_inverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Checks for saved inverse matrix, 
+## if present use it, otherwise compute it
+##
+cacheSolve <- function(x) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$get_inverse()
+  if(!is.null(m)){
+    message("using cached version")
+    return(m)
+  }
+  d <- x$get()
+  m <- solve(d)
+  x$set_inverse(m)
+  m
 }
